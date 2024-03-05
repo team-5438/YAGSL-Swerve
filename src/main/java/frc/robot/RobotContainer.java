8891111120c809c1cb02bb89198a34da0cccd3d0
Squibid <me@zacharyscheiman.com>
2024-03-05 14:07:57 -0500

    fix indentation in RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,7 +23,6 @@ import frc.robot.commands.AimShooter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.DriverConstants;
 import frc.robot.commands.IntakeCommand;
-import frc.robot.commands.IntakeCommandReverse;
 import frc.robot.commands.LEDCommand;
 import frc.robot.commands.ShootCommand;
 import frc.robot.subsystems.AmpSubsystem;
@@ -49,27 +48,38 @@ import frc.robot.subsystems.ClimberSubsystem;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  public final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
-  public final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   public final LimelightSubsystem limelightSubsystem = new LimelightSubsystem();
   public final AmpSubsystem ampSubsystem = new AmpSubsystem();
   private final LEDSubsystem ledSubsystem = new LEDSubsystem();
 
-  public final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
+  private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
   private final ClimberSubsystem climberSubsystem  = new ClimberSubsystem();
 
-  public final XboxController driver = new XboxController(Constants.DriverConstants.id);
+  private final XboxController driver = new XboxController(Constants.DriverConstants.id);
   public final PS4Controller operator = new PS4Controller(Constants.OperatorConstants.id);
-  public double shootSpeed = 0.1;
   double pov, speedMod = 1;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-
+  /* The container for the robot. Contains subsystems, OI devices, and commands. */
   private final AlignWithSpeaker alignWithSpeaker = new AlignWithSpeaker(limelightSubsystem, drivebase);
-  private final AimShooter aimShooter = new AimShooter(shooterSubsystem, limelightSubsystem, operator, shootSpeed);
+  private final AimShooter aimShooter = new AimShooter(shooterSubsystem, limelightSubsystem, operator, 0.1);
   private final AmpPivot ampPivot = new AmpPivot(ampSubsystem, shooterSubsystem, operator);
 
   public RobotContainer() {
+    // Named Commands must be the first code called in RobotContainer
+    /*NamedCommands.registerCommand("Intake", new RevShooterWheels(shooterSubsystem).andThen(
+        new SequentialCommandGroup(
+        new IntakeCommand(intakeSubsystem, shooterSubsystem),
+        new InstantCommand(() -> shooterSubsystem.feedMotor.set(-0.21)),
+        new WaitCommand(0.04),
+        new InstantCommand(() -> shooterSubsystem.feedMotor.set(0)),
+        new InstantCommand(() -> shooterSubsystem.speakerMotorBottom.set(0)),
+        new InstantCommand(() -> shooterSubsystem.speakerMotorTop.set(0))
+        ))); */
+    NamedCommands.registerCommand("Intake", new InstantCommand(() -> System.out.println("Intaking...")));
+    NamedCommands.registerCommand("AutoAim", new InstantCommand(() -> System.out.println("Auto Aiming Wheels")));
+    NamedCommands.registerCommand("Shoot", new ShootCommand(shooterSubsystem, intakeSubsystem).withTimeout(1));
 
     // The robot's subsystems and commands are defined here...
     // Configure the trigger bindings
@@ -80,10 +90,10 @@ public class RobotContainer {
     DoubleSupplier angularRotationX = () -> driver.getRawAxis(4) / (driver.getRawAxis(XboxController.Axis.kLeftTrigger.value) + 1);
 
     Command driveFieldOrientedAnglularVelocity = new InstantCommand(() -> drivebase.drive(
-        new Translation2d(Math.pow(translationX.getAsDouble(), 3) * drivebase.swerveDrive.getMaximumVelocity(),
-            Math.pow(translationY.getAsDouble(), 3) * drivebase.swerveDrive.getMaximumVelocity()),
-        Math.pow(angularRotationX.getAsDouble(), 3) * drivebase.swerveDrive.getMaximumAngularVelocity(),
-       true), drivebase);
+      new Translation2d(Math.pow(translationX.getAsDouble(), 3) * drivebase.swerveDrive.getMaximumVelocity(),
+        Math.pow(translationY.getAsDouble(), 3) * drivebase.swerveDrive.getMaximumVelocity()),
+      Math.pow(angularRotationX.getAsDouble(), 3) * drivebase.swerveDrive.getMaximumAngularVelocity(),
+      true), drivebase);
     // Switch for driveFieldOrientedDirectAngleSim if thats needed
     drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
     ampSubsystem.setDefaultCommand(ampPivot);
@@ -92,7 +102,7 @@ public class RobotContainer {
 
   private void configureBindings() {
     NamedCommands.registerCommand("Intake", new RevShooterWheels(shooterSubsystem, 1.0).andThen(
-     new SequentialCommandGroup(
+      new SequentialCommandGroup(
         new InstantCommand(() -> System.out.println("Intaking...")),
         new IntakeCommand(intakeSubsystem, shooterSubsystem),
         new InstantCommand(() -> shooterSubsystem.feedMotor.set(-0.21)),
@@ -100,7 +110,7 @@ public class RobotContainer {
         new InstantCommand(() -> shooterSubsystem.feedMotor.set(0)),
         new InstantCommand(() -> shooterSubsystem.speakerMotorBottom.set(0)),
         new InstantCommand(() -> shooterSubsystem.speakerMotorTop.set(0))
-    )));
+      )));
     NamedCommands.registerCommand("Shoot", new ShootCommand(shooterSubsystem, intakeSubsystem).withTimeout(1));
 
     new JoystickButton(driver, XboxController.Button.kY.value).onTrue(new InstantCommand(drivebase::zeroGyro));
@@ -109,32 +119,32 @@ public class RobotContainer {
 
     // new JoystickButton(operator, PS4Controller.Button.kL2.value).whileTrue(new RevShooterWheels(shooterSubsystem, shootSpeed));
     new JoystickButton(operator, PS4Controller.Button.kSquare.value).onTrue(new SequentialCommandGroup(
-        new IntakeCommand(intakeSubsystem, shooterSubsystem).withTimeout(5),
-        new InstantCommand(() -> shooterSubsystem.feedMotor.set(-0.21)),
-        new WaitCommand(0.04),
-        new InstantCommand(() -> shooterSubsystem.feedMotor.set(0))
+      new IntakeCommand(intakeSubsystem, shooterSubsystem).withTimeout(5),
+      new InstantCommand(() -> shooterSubsystem.feedMotor.set(-0.21)),
+      new WaitCommand(0.04),
+      new InstantCommand(() -> shooterSubsystem.feedMotor.set(0))
     ));
     RevShooterWheels shootFast = new RevShooterWheels(shooterSubsystem, 0.9);
     ShootCommand feed = new ShootCommand(shooterSubsystem, intakeSubsystem);
     new JoystickButton(operator, PS4Controller.Button.kR2.value).onTrue(new SequentialCommandGroup(
-        new InstantCommand(() -> shootFast.schedule()),
-        new WaitCommand(0.5),
-        new InstantCommand(() -> feed.schedule()),
-        new WaitCommand(0.5),
-        new InstantCommand(() -> shootFast.cancel()),
-        new InstantCommand(() -> feed.cancel())
+      new InstantCommand(() -> shootFast.schedule()),
+      new WaitCommand(0.5),
+      new InstantCommand(() -> feed.schedule()),
+      new WaitCommand(0.5),
+      new InstantCommand(() -> shootFast.cancel()),
+      new InstantCommand(() -> feed.cancel())
     ));
     // comment out the following RevShooterWheels and sequential command to go back to L2 defualt shooting
     RevShooterWheels shootSlow = new RevShooterWheels(shooterSubsystem, Constants.Shooter.shooterRevSpeed);
     new JoystickButton(operator, PS4Controller.Button.kL2.value).onTrue(new SequentialCommandGroup(
-        new InstantCommand(() -> shootSlow.schedule()),
-        new WaitCommand(3),
-        new InstantCommand(() -> feed.schedule()),
-        new WaitCommand(0.5),
-        new InstantCommand(() -> shootSlow.cancel()),
-        new InstantCommand(() -> feed.cancel())
+      new InstantCommand(() -> shootSlow.schedule()),
+      new WaitCommand(3),
+      new InstantCommand(() -> feed.schedule()),
+      new WaitCommand(0.5),
+      new InstantCommand(() -> shootSlow.cancel()),
+      new InstantCommand(() -> feed.cancel())
     ));
-      
+
 
 
     // new JoystickButton(operator, PS4Controller.Button.kCross.value).onTrue(new SequentialCommandGroup(
@@ -147,11 +157,10 @@ public class RobotContainer {
     new JoystickButton(operator, PS4Controller.Button.kOptions.value).whileTrue(new ClimbCommand(climberSubsystem, 0.5));
     new JoystickButton(operator, PS4Controller.Button.kShare.value).whileTrue(new ClimbCommand(climberSubsystem, -0.5));
     new JoystickButton(operator, PS4Controller.Button.kTouchpad.value).onTrue(new SequentialCommandGroup(
-      new InstantCommand(() -> LEDSubsystem.sponsorStrip1.setData(LEDCommand.setStripColor(27, 0, 0, 255))),
+      new InstantCommand(() -> LEDSubsystem.led0.setData(LEDCommand.setStripColor(27, 0, 0, 255))),
       new WaitCommand(1),
-      new InstantCommand(() -> LEDSubsystem.sponsorStrip1.setData(LEDCommand.setStripColor(27, 0, 0, 0)))
+      new InstantCommand(() -> LEDSubsystem.led0.setData(LEDCommand.setStripColor(27, 0, 0, 0)))
     ));
-    new JoystickButton(operator, PS4Controller.Button.kPS.value).whileTrue(new IntakeCommandReverse(intakeSubsystem, shooterSubsystem));
   }
 
   public Command getAutonomousCommand() {
