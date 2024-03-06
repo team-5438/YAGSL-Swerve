41b71fef99914e61f1c75714bafb64fe81b627df
EggbertFluffle <hdiambrosio@gmail.com>
2024-03-06 09:54:43 -0500

    rewrote and changed LED logic

--- src/main/java/frc/robot/commands/AlignWithSpeaker.java
+++ src/main/java/frc/robot/commands/AlignWithSpeaker.java
@@ -2,41 +2,40 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class AlignWithSpeaker extends Command {
-    LimelightSubsystem ll;
+    LimelightSubsystem limelightSubsystem;
     boolean aligned;
-    Translation2d zero;
-    SwerveSubsystem swerve;
+    Translation2d translationZero;
+    SwerveSubsystem swerveSubsystem;
+	double alignedTolerance;
     double rotSpeed;
-    double tolerance;
     double offsetTagOffset;
     PIDController rotationPID;
 
-    public AlignWithSpeaker(LimelightSubsystem _limelight, SwerveSubsystem _swerve) {
-        ll = _limelight;
-        swerve = _swerve;
-        tolerance = 2.5;
-        zero = new Translation2d(0, 0);
-        rotationPID = new PIDController(0.5, 0.0, 0.00);
-        this.addRequirements(swerve);
+    public AlignWithSpeaker(LimelightSubsystem limelightSubsystem, SwerveSubsystem swerveSubsystem) {
+        this.addRequirements(swerveSubsystem);
+        this.limelightSubsystem = limelightSubsystem;
+        this.swerveSubsystem = swerveSubsystem;
+        translationZero = new Translation2d(0, 0);
+        rotationPID = new PIDController(0.5, 0.0, 0.0);
+		alignedTolerance = 2.5;
     }
 
     @Override
     public void execute() {
-        rotSpeed = rotationPID.calculate(ll.tx, 0.0) / 6;
-        // rotSpeed = ll.tx > 0 ? -0.2 : 0.2;
-        swerve.drive(zero, -rotSpeed, false);
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        LEDSubsystem.led0.setData(LEDCommand.setStripColor(27, 0, 255, 0));
+        rotSpeed = rotationPID.calculate(limelightSubsystem.tx, 0.0) / 6;
+        swerveSubsystem.drive(translationZero, -rotSpeed, false);
+		
+		// Set LEDs to orange while ALIGNING, green when ALIGNED
+		if(Math.abs(limelightSubsystem.tx) < alignedTolerance) {
+			LEDSubsystem.led0.setData(LEDCommand.setStripColor(27, 0, 255, 0));
+		} else {
+			LEDSubsystem.led0.setData(LEDCommand.setStripColor(27, 255, 165, 0));
+		}
     }
 }
